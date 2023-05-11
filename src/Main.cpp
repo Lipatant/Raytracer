e77a5045510325d2a97ecb970caf15057593001e
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2023-05-11 13:41:08 +0200

    feat: parser now working with arguments

--- src/Main.cpp
+++ src/Main.cpp
@@ -34,69 +34,20 @@ int main(int const ac, char * const * const av)
 {
     Raytracer::Ray ray;
     Raytracer::Scene scene;
-    Parser::File test;
+    Parser::File fileScene;
 
-//    if (!Arg::INPUT.setArguments(ac, av))
-//        return 84;
+    if (!Arg::INPUT.setArguments(ac, av))
+        return 84;
     scene.camera.width = Arg::INPUT.width;
     scene.camera.height = Arg::INPUT.height;
     scene.camera.fov = 180;
     scene.camera.position = Math::Point3D(-1.9,0,0);
     scene.camera.rotation = Math::Angle3D(0,0);
-    test.parseFile(av[1]);
-    scene.shapes.splice(scene.shapes.end(), test.shapes);
-    std:: cout << scene.shapes.size() << " - " << test.shapes.size() << std::endl;
-    /*
-    scene.shapes.push_back(Shape::createShape<Shape::Sphere>( \
-        Math::Point3D(0,0,2), 0.5, Raytracer::Texture(Raytracer::Color(1, 1, 1), Raytracer::Color(1, 1, 1, 1))));
-    scene.shapes.push_back(Shape::createShape<Shape::Triangle>( \
-        Math::Point3D(0,0,0), Math::Point3D(0,1,1), Math::Point3D(1,1,0), \
-        Raytracer::Texture(Raytracer::Texture(Raytracer::Color(1, 0.5, 1), \
-        Raytracer::Color(1, 0.5, 1, 0.2), 0.5))));
-    scene.shapes.push_back(Shape::createShape<Shape::Triangle>( \
-        Math::Point3D(0,0,0), Math::Point3D(-1,-1,0), Math::Point3D(0,-1,-1), \
-        Raytracer::Texture(Raytracer::Texture(Raytracer::Color(1, 1, 0.5), \
-        Raytracer::Color(1, 1, 0.5, 0.2), 0.5))));
-    scene.shapes.push_back(Shape::createShape<Shape::Sphere>( \
-        Math::Point3D(0,0,0), 0.05, Raytracer::Texture(Raytracer::Color(1, 0.5, 0.5), Raytracer::Color(1, 0.5, 0.5, 0.2))));
-    scene.shapes.push_back(Shape::createShape<Shape::Sphere>( \
-        Math::Point3D(1,1,0), 0.05, Raytracer::Texture(Raytracer::Color(0.5, 1, 0.5), Raytracer::Color(0.5, 1, 0.5, 0.2))));
-    scene.shapes.push_back(Shape::createShape<Shape::Sphere>( \
-        Math::Point3D(0,1,1), 0.05, Raytracer::Texture(Raytracer::Color(0.5, 0.5, 1), Raytracer::Color(0.5, 0.5, 1, 0.2))));
-    scene.shapes.push_back(Shape::createShape<Shape::Sphere>( \
-        Math::Point3D(-1,-1,0), 0.05, Raytracer::Texture(Raytracer::Color(0.5, 1, 0.5), Raytracer::Color(0.5, 1, 0.5, 0.2))));
-    scene.shapes.push_back(Shape::createShape<Shape::Sphere>( \
-        Math::Point3D(0,-1,-1), 0.05, Raytracer::Texture(Raytracer::Color(0.5, 0.5, 1), Raytracer::Color(0.5, 0.5, 1, 0.2))));
-    scene.shapes.push_back(Shape::createShape<Shape::Sphere>(
-        Math::Point3D(0,0,0), 0.8, Raytracer::Texture(Raytracer::Color(1, 1, 1))));
-    // Haut
-    scene.shapes.push_back(Shape::createShape<Shape::Plane>( \
-        Math::Point3D(0,0,2), Math::Vector3D(1,0,0), Math::Vector3D(0,1,0), \
-        Raytracer::Texture(Raytracer::Color(1, 1, 1), Raytracer::Color(1, 1, 1, 0.2), DEFAULT_MIRROR_CUBE)));
-    // Bas
-    scene.shapes.push_back(Shape::createShape<Shape::Plane>( \
-        Math::Point3D(0,0,-2), Math::Vector3D(1,0,0), Math::Vector3D(0,1,0), \
-        Raytracer::Texture(Raytracer::Color(1, 1, 1), Raytracer::Color(1, 1, 1, 0), DEFAULT_MIRROR_CUBE)));
-    // Arrière
-    scene.shapes.push_back(Shape::createShape<Shape::Plane>( \
-        Math::Point3D(2,0,0), Math::Vector3D(0,1,0), Math::Vector3D(0,0,1), \
-        Raytracer::Texture(Raytracer::Color(0.5, 0.5, 1), DEFAULT_MIRROR_CUBE)));
-    // Avant
-    scene.shapes.push_back(Shape::createShape<Shape::Plane>( \
-        Math::Point3D(-2,0,0), Math::Vector3D(0,1,0), Math::Vector3D(0,0,1), \
-        Raytracer::Texture(Raytracer::Color(1, 1, 1), DEFAULT_MIRROR_CUBE)));
-    // Gauche
-    scene.shapes.push_back(Shape::createShape<Shape::Plane>( \
-        Math::Point3D(0,-2,0), Math::Vector3D(1,0,0), Math::Vector3D(0,0,1), \
-        Raytracer::Texture(Raytracer::Color(1, 0.5, 0.5), DEFAULT_MIRROR_CUBE)));
-    // Droite
-    scene.shapes.push_back(Shape::createShape<Shape::Plane>( \
-        Math::Point3D(0,2,0), Math::Vector3D(1,0,0), Math::Vector3D(0,0,1), \
-        Raytracer::Texture(Raytracer::Color(0.5, 1, 0.5), DEFAULT_MIRROR_CUBE)));
-    */
-//    if (Arg::INPUT.out.empty())
-//        std::cout << scene.render();
-//    else
+    fileScene.parseFile(Arg::INPUT.fileScene.c_str());
+    scene.shapes.splice(scene.shapes.end(), fileScene.shapes);
+    if (Arg::INPUT.out.empty())
+        std::cout << scene.render();
+    else
         writeFile(Arg::INPUT.out, scene.render());
     return 0;
 }
